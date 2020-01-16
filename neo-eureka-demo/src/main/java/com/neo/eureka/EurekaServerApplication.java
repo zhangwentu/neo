960790cf44a3a113e49ebc0f3e9a02ead888bd31
zhangwt <zhangwentu@oswaldlink.com>
2020-01-16 17:26:12 +0800

    spring-cloud init

---
+++ neo-eureka-demo/src/main/java/com/neo/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.neo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p></p>
+ *
+ * @author neo
+ * @date 2020/1/16 15:32
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
